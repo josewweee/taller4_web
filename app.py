023d2123d83a3efd5e8fe67fad6e860ae7d4d483
josewweee <josemb125@gmail.com>
2019-10-23 10:40:59 -0500

    agregando web para crear y ver objetos

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request, render_template
+import requests
+ 
+app = Flask(__name__, template_folder='templates')
+ 
+variables_list = ['Sensor en terreno', 'Imagen satelital', 'Imagen dron', 'Dato derivado']
+
+
+@app.route('/crearSensor', methods=['GET'])
+def crearSensor():
+    return render_template('crearSensor.html', variables=variables_list) 
+
+@app.route('/listarSensores', methods=['GET'])
+def listarSensores():
+    sensores_list = requests.get('http://localhost:5000/mediciones').json()
+    return render_template('listarSensores.html', mediciones=sensores_list)
+
+
+@app.route('/guardarSensor', methods=['POST'])
+def guardarSensor():
+        medicion = dict(request.values)
+        medicion['valor'] = int(medicion['valor'])
+        requests.post('http://localhost:5000/mediciones',json=medicion)
+        return (listarSensores())
+        
+        
+#PUERTO Y AUTORUN
+''' quitamos el app run para que la app corra en azure '''
+''' app.run(port=8000,debug=True) '''
